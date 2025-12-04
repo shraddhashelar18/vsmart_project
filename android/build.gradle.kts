ceea786efa32cd9a0ee4d6e742064a384632b94f
kaverikadwadkar <kaverikadwadkar1107@gmail.com>
2025-12-04 16:16:41 +0530

    13th commit

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    // Firebase Google Services Plugin (not applied here)
+    id("com.google.gms.google-services") apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -9,13 +14,14 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
+
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
+
     project.evaluationDependsOn(":app")
 }
 
